efbbb70b33111069e6c21a3259aeb6991fb8c8de
pradeepkmkm-design <pradeepkmkm@gmail.com>
2025-10-23 16:12:07 +0530

    FILES ADDED

---
+++ Task-1.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter first number "))
+num2 = float(input("Enter Second number "))
+Sum = num1 + num2
+Diff = num1 - num2
+Mul = num1 * num2
+Div = num1 / num2
+
+print("Sum of two numbers is" ,Sum)
+print("Subtraction of two numbers is" ,Diff)
+print("Multiplication  of two numbers is" ,Mul)
+print("Division of two numbers is" ,Div)
---
+++ Task-2.py
@@ -0,0 +1,3 @@
+First_name = input("Enter your first name:")
+Second_name = input("Enter your Second name:")
+print("Hello",First_name+' '+Second_name +"! Welcome to the PYTHON programming.")
